0cdb719b6bceccb57e1dc7cb2886652bb6f07325
tiurmakinantinababan <tiurmakinantinababan15@gmail.com>
2026-07-07 23:41:48 +0700

    Add files via upload

---
+++ welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="id">
+     <head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initialscale=
+1.0">
+ <title>Toko Kita</title>
+ <style>
+ body {
+ font-family: Arial, sans-serif;
+ text-align: center;
+ margin-top: 100px;
+ background-color: #f3f4f6;
+ }
+
+
+ .container {
+ background-color: white;
+ padding: 30px;
+ border-radius: 10px;
+ box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+ display: inline-block;
+ }
+ h1 { color: #ff2d20; }
+ </style>
+ </head>
+ <body>
+ <div class="container">
+ <h1>Selamat Datang di Toko Kita!</h1>
+ <p>Nama: Nathaliaa</p>
+<p>NIM: 224520057</p>
+ <p>Aplikasi ini dibuat menggunakan Laravel 12.</p>
+ </div>
+ </body>
+ </html>
